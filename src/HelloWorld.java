cf81182a387138c8dff03fbbd2fa0e2b309f2699
Mong-Gu <yhw8258@naver.com>
2020-12-30 12:34:23 +0900

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String message = "20201230 java start, ";
+        System.out.println(message + "Hello World!");
+    }
+}
